d37ad573abc6ea9cff5a5888840dc3c1fcb34df0
Oleg Sokolov <olegsokolov190@44-4.ru>
2022-05-11 11:30:44 +0600

    Changed inheritance in the Alex class

--- src/main/java/com/example/Alex.java
+++ src/main/java/com/example/Alex.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 public class Alex extends Lion {
 
-    private final Lion lion;
     private final List<String> friends = List.of("Марти", "Глория", "Мелман");
     private final String location = "Нью-Йоркский зоопарк";
 
-    public Alex(Lion lion) throws Exception {
-        super("Самец");
-        this.lion = lion;
+    public Alex(Feline feline) throws Exception {
+        super("Самец", feline);
     }
 
     @Override
@@ -21,10 +19,6 @@ public class Alex extends Lion {
     @Override
     public boolean doesHaveMane() {
         return true;
-    }
-
-    public List<String> getFood() throws Exception {
-        return lion.getFood();
     }
 
     public List<String> getFriends() {
