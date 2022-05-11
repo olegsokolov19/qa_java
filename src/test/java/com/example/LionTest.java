723adb28ce333c462d0a306f4478e00a3174e173
Oleg Sokolov <olegsokolov190@44-4.ru>
2022-05-11 11:41:16 +0600

    Fixed test for Lion class

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,10 +1,13 @@
 package com.example;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -14,6 +17,9 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class LionTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
 
     private final String sex;
     private Lion lion;
@@ -26,6 +32,9 @@ public class LionTest {
         this.expectedHasManeResult = expectedHasManeResult;
     }
 
+    @Mock
+    Feline feline;
+
     @Parameterized.Parameters
     public static Object[][] getHasMane() {
         return new Object[][] {
@@ -34,9 +43,15 @@ public class LionTest {
         };
     }
 
+    @Before
+    public void setUp() throws Exception {
+        lion = new Lion(sex, feline);
+    }
+
     @Test
-    public void shouldReturnOneKittens() throws Exception {
-        lion = new Lion(sex);
+    public void shouldReturnOneKittens() {
+        Mockito.when(feline.getKittens()).thenReturn(1);
+
         int expected = 1;
         int actual = lion.getKittens();
 
@@ -44,8 +59,7 @@ public class LionTest {
     }
 
     @Test
-    public void shouldHaveManeTrue() throws Exception {
-        lion = new Lion(sex);
+    public void shouldHaveManeTrue() {
         boolean actual = lion.doesHaveMane();
 
         assertEquals(expectedHasManeResult, actual);
@@ -53,14 +67,15 @@ public class LionTest {
 
     @Test
     public void shouldThrowExceptionWithIncorrectMane() {
-        String actual = Assert.assertThrows(Exception.class, () -> new Lion("Другое")).getMessage();
+        String actual = Assert.assertThrows(Exception.class, () -> new Lion("Другое", feline)).getMessage();
 
         assertEquals(expectedError, actual);
     }
 
     @Test
     public void shouldReturnPredator() throws Exception {
-        lion = new Lion(sex);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
         List<String> expected = List.of("Животные", "Птицы", "Рыба");
         List<String> actual = lion.getFood();
 
